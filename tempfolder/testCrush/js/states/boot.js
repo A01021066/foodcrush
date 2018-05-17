569549a5fd0c3f1a5d30ea47240b136569143bb3
nickybittencourt <nickytuon75@gmail.com>
2018-05-17 13:13:51 -0700

    full game

--- tempfolder/testCrush/js/states/boot.js
+++ tempfolder/testCrush/js/states/boot.js
@@ -1,25 +1,23 @@
 var Match3 = Match3 || {};
 
+//setting game configuration and loading the assets for the loading screen
 Match3.BootState = {
+  init: function() {
+    //loading screen will have a white background
+    this.game.stage.backgroundColor = '#fff';
 
-    init: function() {
+    //scaling options
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-        this.game.stage.backgroundColor = '#fff';
-
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-
-        this.scale.pageAlignHorizontally = true;
-        this.scale.pageAlignVertically = true;
-    },
-
-    preload: function() {
-
-        this.load.image('bar', 'assets/images/preload-bar.png');
-    },
-
-    create: function() {
-
-        this.state.start('Preload');
-    }
-
+    //have the game centered horizontally
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  },
+  preload: function() {
+    //assets we'll use in the loading screen
+    this.load.image('bar', 'assets/images/preloader-bar.png');
+  },
+  create: function() {
+    this.state.start('Preload');
+  }
 };
